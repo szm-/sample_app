2c093d7f1dbde585ebcfd608460daf6f2754d59d
szm <szm-@mail.com>
2012-08-12 17:02:10 +0100

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -8,6 +8,9 @@ gem 'rails', '3.2.7'
 group :development, :test do
     gem 'sqlite3', '1.3.5'
     gem 'rspec-rails', '2.10.0'
+    gem 'guard-rspec', '0.5.5'
+    gem 'rvm', '>= 1.6.18'
+    gem 'bundler', '>= 1.0.5'
 end
 
 
@@ -21,8 +24,14 @@ end
 
 gem 'jquery-rails', '2.0.2'
 
+# Test gems on Windows
 group :test do
     gem 'capybara', '1.1.2'
+    gem 'rb-fchange', '0.0.5'
+    gem 'rb-notifu', '0.0.4'
+    gem 'win32console', '1.3.0'
+    gem 'guard-spork', '0.3.2'
+    gem 'spork', '0.9.0'
 end
 
 group :production do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "RoR Tutorial Sample App" }
+
   describe "Home page" do
-    
+
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-    
-    it "should have the title 'Home'" do
+
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', 
-                            :text => 'RoR Tutorial Sample App | Home')
+      page.should have_selector('title', :text => 'RoR Tutorial Sample App')
+    end
+
+    it "should not have a custom title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -22,25 +28,36 @@ describe "Static pages" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
-    
+
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', 
-                            :text => 'RoR Tutorial Sample App | Help')
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
   describe "About page" do
-    
+
     it "should have the h1 'About us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About us')
     end
-    
+
     it "should have the title 'About us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', 
-                        :text => 'RoR Tutorial Sample App | About us')
+      page.should have_selector('title', :text => "#{base_title} | About us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 end
